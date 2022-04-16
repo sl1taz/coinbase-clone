bffe7c2a989cd579db9766ff78e9118c64a8fc2f
pakavi <pakavi@protonmail.ch>
2022-04-16 10:34:44 +0300

    Setup token schema

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,8 +1,10 @@
 import createSchema from "part:@sanity/base/schema-creator";
 import schemaTypes from "all:part:@sanity/base/schema-type";
 
+import { token } from "./token.js";
+
 
 export default createSchema({
   name: "default",
-  types: schemaTypes.concat([]),
+  types: schemaTypes.concat([token]),
 });
---
+++ studio/schemas/token.js
@@ -0,0 +1,32 @@
+export const token = {
+  name: "coins",
+  title: "Coins",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "symbol",
+      title: "Symbol",
+      type: "string",
+    },
+    {
+      name: "contractAddress",
+      title: "Contract Address",
+      type: "string",
+    },
+    {
+      name: "usdPrice",
+      title: "USD Price",
+      type: "string",
+    },
+    {
+      name: "logo",
+      title: "Logo",
+      type: "string",
+    },
+  ],
+};
